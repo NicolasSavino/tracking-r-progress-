dcd8edd6d51c9df0b2c6ebaf36e7219a570ef039
Nicolas Savino <nicolassavino3@gmail.com>
2025-06-02 17:31:59 -0400

    Add files via upload

---
+++ Practice Journal for R.R
@@ -0,0 +1,28 @@
+# Swirl: Lesson 1 - Basic Building Blocks
+
+# Basic arithmetic
+5 + 7  # [1] 12
+
+# Variable assignment
+x <- 5 + 7
+x        # [1] 12
+
+# Use variable in expression
+y <- x - 3
+y        # [1] 9
+
+# Create a numeric vector
+z <- c(1.1, 9, 3.14)
+z        # [1] 1.10 9.00 3.14
+
+# Help documentation (optional)
+# ?c
+
+# Combine vectors
+c(z, 555, z)  # [1] 1.10 9.00 3.14 555.00 1.10 9.00 3.14
+
+# Arithmetic with vectors
+z * 2 + 100   # [1] 102.20 118.00 106.28
+
+# Math functions
+my_sqrt_
